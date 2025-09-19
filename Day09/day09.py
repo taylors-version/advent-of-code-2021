9c2c7f33335d3e75e4d6d4b54013a7c0e7970465
Ben Healy <139165148+taylors-version@users.noreply.github.com>
2025-09-19 16:49:00 +0100

    Start part 2

--- Day09/day09.py
+++ Day09/day09.py
@@ -1,10 +1,10 @@
-from functools import reduce
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
 def puzzle1(data):
     numbers = [[int(x) for x in list(y)] for y in data]
-
-    ben = [2,3]
-    bob = numbers[ben[1]][ben[0]]
 
     result = 0
     for y in range(len(numbers)):
@@ -24,6 +24,25 @@ def puzzle1(data):
     return result
 
 def puzzle2(data):
+    numbers = [[int(x) for x in list(y)] for y in data]
+
+    lows = []
+    for y in range(len(numbers)):
+        for x in range(len(numbers[y])):
+            up, down, left, right = True, True, True, True
+            if y > 0:
+                up = numbers[y][x] < numbers[y-1][x]
+            if y + 1 < len(numbers):
+                down = numbers[y][x] < numbers[y+1][x]
+            if x > 0:
+                left = numbers[y][x] < numbers[y][x-1]
+            if x + 1 < len(numbers[y]):
+                right = numbers[y][x] < numbers[y][x+1]
+            if up and down and left and right:
+                lows.append(Point(x, y))
+
+    for p in lows:
+        ben = ""
     return 0
 
 if __name__ == '__main__':
