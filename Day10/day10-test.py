9d7174509ac0cb3b3c4ee2667768441593d6dbad
Ben Healy <139165148+taylors-version@users.noreply.github.com>
2025-09-24 11:29:51 +0100

    Day 10 part 2

--- Day10/day10-test.py
+++ Day10/day10-test.py
@@ -14,15 +14,26 @@ class MyTestCase(unittest.TestCase):
 <{([{{}}[<[[[<>{}]]]>[]]"""
 
     def test_puzzle1(self):
-        self.assertEqual(0, day10.line_check("()"))
-        self.assertEqual(25137, day10.line_check("(>"))
-        self.assertEqual(1197, day10.line_check("(}"))
-        self.assertEqual(0, day10.line_check("(()"))
-        self.assertEqual(1197, day10.line_check("((}"))
-        self.assertEqual(0, day10.line_check("(())"))
-        self.assertEqual(0, day10.line_check("(())"))
-        self.assertEqual(3, day10.line_check("[[<[([]))<([[{}[[()]]]"))
+        self.assertEqual(0, day10.puzzle1(["()"]))
+        self.assertEqual(25137, day10.puzzle1(["(>"]))
+        self.assertEqual(1197, day10.puzzle1(["(}"]))
+        self.assertEqual(0, day10.puzzle1(["(()"]))
+        self.assertEqual(1197, day10.puzzle1(["((}"]))
+        self.assertEqual(0, day10.puzzle1(["(())"]))
+        self.assertEqual(0, day10.puzzle1(["(())"]))
+        self.assertEqual(3, day10.puzzle1(["[[<[([]))<([[{}[[()]]]"]))
         self.assertEqual(26397, day10.puzzle1(self.data.splitlines()))
+
+    def test_puzzle2(self):
+        self.assertEqual("", day10.missing_chars(""))
+        self.assertEqual(")", day10.missing_chars("("))
+        self.assertEqual("]", day10.missing_chars("["))
+        self.assertEqual("])", day10.missing_chars("(["))
+        self.assertEqual(")", day10.missing_chars("([]"))
+        self.assertEqual("", day10.missing_chars("{([(<{}[<>[]}>{[]{[(<()>"))
+        self.assertEqual("}}]])})]", day10.missing_chars("[({(<(())[]>[[{[]{<()<>>"))
+        self.assertEqual(294, day10.puzzle2(["<{([{{}}[<[[[<>{}]]]>[]]"]))
+        self.assertEqual(288957, day10.puzzle2(self.data.splitlines()))
 
 
 if __name__ == '__main__':
--- Day10/day10.py
+++ Day10/day10.py
@@ -1,11 +1,15 @@
+from functools import reduce
+
 matches = {"(":")", "[":"]", "{":"}", "<":">"}
-scores = {")":3, "]":57, "}":1197, ">":25137}
+illegal_scores = {")":3, "]":57, "}":1197, ">":25137}
+completion_scores = {")":1, "]":2, "}":3, ">":4}
 def puzzle1(data):
 
     return sum([line_check(line) for line in data])
 
 def puzzle2(data):
-    return 0
+    scores = sorted(list(filter(lambda x: x!=0,[reduce(lambda x, y: x*5 + y, line, 0) for line in [[completion_scores[c] for c in missing_chars(line)] for line in data]])))
+    return scores[len(scores)//2]
 
 def line_check(line):
     l = []
@@ -15,8 +19,22 @@ def line_check(line):
         else:
             m = l.pop()
             if c != matches[m]:
-                return scores[c]
+                return illegal_scores[c]
     return 0
+
+def missing_chars(line):
+    if len(line) == 0:
+        return ""
+    l = []
+    for c in line:
+        if c in matches:
+            l.insert(0,matches[c])
+        elif c == l[0]:
+            l.pop(0)
+        else:
+            return ""
+    return ''.join(l)
+
 
 if __name__ == '__main__':
     with open("input.txt") as f:
