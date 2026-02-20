90f7939cc0f8877a301c0ce10f26e0ed9a00373b
Ben Healy <139165148+taylors-version@users.noreply.github.com>
2026-02-20 13:36:51 +0000

    part 1

--- Day18/day18-test.py
+++ Day18/day18-test.py
@@ -1,4 +1,5 @@
 import unittest
+import Day18.day18
 from Day18.day18 import SnailFish
 
 
@@ -33,10 +34,44 @@ class MyTestCase(unittest.TestCase):
         b = SnailFish.from_string("[[3,4],5]")
         added = a.add(b)
         self.assertEqual("[[1,2],[[3,4],5]]", str(added))
-        a = SnailFish.from_string("[[3,[2,[1,[17,13]]]]")
-        b = SnailFish.from_string("[6,[5,[4,3]]]")
-        added = a.add(b) #[[3,[2,[1,[17,13]]]],[6,[5,[4,3]]]]
-        #self.assertEqual("[[3,[2,[18,0]]],[19,[5,[4,3]]]]", str(added))
+        a = SnailFish.from_string("[[[[4,3],4],4],[7,[[8,4],9]]]")
+        b = SnailFish.from_string("[1,1]")
+        added = a.add(b)
+        self.assertEqual("[[[[0,7],4],[[7,8],[6,0]]],[8,1]]", str(added))
+
+    def many_adding(self):
+        sf = SnailFish.from_string("[1,1]")
+        sf = sf.add(SnailFish.from_string("[2,2]"))
+        sf = sf.add(SnailFish.from_string("[3,3]"))
+        sf = sf.add(SnailFish.from_string("[4,4]"))
+        sf = sf.add(SnailFish.from_string("[5,5]"))
+        sf = sf.add(SnailFish.from_string("[6,6]"))
+        self.assertEqual("[[[[5,0],[7,4]],[5,5]],[6,6]]", str(sf))
+
+        sf = SnailFish.from_string("[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]")
+        sf = sf.add(SnailFish.from_string("[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]"))
+        sf = sf.add(SnailFish.from_string("[[2,[[0,8],[3,4]]],[[[6,7],1],[7,[1,6]]]]"))
+        sf = sf.add(SnailFish.from_string("[[[[2,4],7],[6,[0,5]]],[[[6,8],[2,8]],[[2,1],[4,5]]]]"))
+        sf = sf.add(SnailFish.from_string("[7,[5,[[3,8],[1,4]]]]"))
+        sf = sf.add(SnailFish.from_string("[[2,[2,2]],[8,[8,1]]]"))
+        sf = sf.add(SnailFish.from_string("[2,9]"))
+        sf = sf.add(SnailFish.from_string("[1,[[[9,3],9],[[9,0],[0,7]]]]"))
+        sf = sf.add(SnailFish.from_string("[[[5,[7,4]],7],1]"))
+        sf = sf.add(SnailFish.from_string("[[[[4,2],2],6],[8,7]]"))
+        self.assertEqual("[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]", str(sf))
+
+    def test_puzzle1(self):
+        sample_input = """[[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]
+[[[5,[2,8]],4],[5,[[9,9],0]]]
+[6,[[[6,2],[5,6]],[[7,6],[4,7]]]]
+[[[6,[0,7]],[0,9]],[4,[9,[9,0]]]]
+[[[7,[6,4]],[3,[1,3]]],[[[5,5],1],9]]
+[[6,[[7,3],[3,2]]],[[[3,8],[5,7]],4]]
+[[[[5,4],[7,7]],8],[[8,3],8]]
+[[9,3],[[9,9],[6,[4,9]]]]
+[[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]
+[[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]"""
+        self.assertEqual(4140, Day18.day18.puzzle1(sample_input.splitlines()))
 
 if __name__ == '__main__':
     unittest.main()
--- Day18/day18.py
+++ Day18/day18.py
@@ -1,5 +1,6 @@
 import itertools
-
+import math
+from functools import reduce
 
 class SnailFish:
     def __init__(self, a, b, depth = 0):
@@ -13,26 +14,101 @@ class SnailFish:
         added = SnailFish(self, sf)
         needs_reducing = True
         while needs_reducing:
-            added_str = str(added)
-            lb = 0
-            i = 0
-            while i < len(added_str):
-                if added_str[i] == '[':
-                    lb += 1
-                elif added_str[i] == ']':
-                    lb -= 1
-                if lb > 4:
-                    comma = next(n for n,c in enumerate(added_str[i:]) if c == ',')
-                    rb = next(n for n,c in enumerate(added_str[i:]) if c == ']')
-                    #l = len(added_str[:i]) - next(n for n,c in enumerate(reversed(added_str[:i])) if is)
-                    l = int(added_str[i+1:i+comma])
-                    r = int(added_str[i+comma+1:i+rb])
-
-                    ben = "hello"
-                    pass
-                i += 1
-            needs_reducing = False
+            path = added.traverse_to_depth4()
+            if path is not None:
+                exploding = added.val_from_path(path)
+                added.explode_left(path, exploding.a)
+                added.explode_right(path, exploding.b)
+                parent = added.val_from_path(path[:-1])
+                if path[-1] == "L":
+                    parent.a = 0
+                else:
+                    parent.b = 0
+            else:
+                path = added.find_val_greater_9()
+                if path is not None:
+                    splitting = added.val_from_path(path)
+                    l = splitting // 2
+                    r = int(math.ceil(splitting/2))
+                    parent = added.val_from_path(path[:-1])
+                    new_sf = SnailFish(l,r)
+                    if path[-1] == "L":
+                        parent.a = new_sf
+                    else:
+                        parent.b = new_sf
+                else:
+                    needs_reducing = False
         return added
+
+    def traverse_to_depth4(self, path=None):
+        if path is None:
+            path = []
+        if len(path) == 4:
+            return path
+        if isinstance(self.a, SnailFish):
+            poss = self.a.traverse_to_depth4(path + ["L"])
+            if poss is not None:
+                return poss
+        if isinstance(self.b, SnailFish):
+            poss = self.b.traverse_to_depth4(path + ["R"])
+            if poss is not None:
+                return poss
+        return None
+
+    def explode_left(self, path, val):
+        left_path = path.copy()
+        left_path.reverse()
+        first_r = next((n for n,p in enumerate(left_path) if p =='R'), None)
+        if first_r is None:
+            return
+        left_path = left_path[first_r+1:]
+        left_path.reverse()
+        left_path.append("L")
+        left = self
+        while isinstance(left, SnailFish):
+            left = self.val_from_path(left_path)
+            left_path.append("R")
+        left_path = left_path[:-1]
+        if left_path[-1] == "R":
+            self.val_from_path(left_path[:-1]).b = left + val
+        else:
+            self.val_from_path(left_path[:-1]).a = left + val
+
+    def explode_right(self, path, val):
+        right_path = path.copy()
+        right_path.reverse()
+        first_l = next((n for n,p in enumerate(right_path) if p =='L'), None)
+        if first_l is None:
+            return
+        right_path = right_path[first_l+1:]
+        right_path.reverse()
+        right_path.append("R")
+        right = self
+        while isinstance(right, SnailFish):
+            right = self.val_from_path(right_path)
+            right_path.append("L")
+        right_path = right_path[:-1]
+        if right_path[-1] == "L":
+            self.val_from_path(right_path[:-1]).a = right + val
+        else:
+            self.val_from_path(right_path[:-1]).b = right + val
+
+    def find_val_greater_9(self, path=None):
+        if path is None:
+            path = []
+        if isinstance(self.a, SnailFish):
+                poss = self.a.find_val_greater_9(path + ["L"])
+                if poss is not None:
+                    return poss
+        if isinstance(self.a, int) and self.a > 9:
+            return path + ["L"]
+        if isinstance(self.b, SnailFish):
+            poss = self.b.find_val_greater_9(path + ["R"])
+            if poss is not None:
+                return poss
+        if isinstance(self.b, int) and self.b > 9:
+            return path + ["R"]
+        return None
 
     def magnitude(self):
         return 3*self.mag_l() + 2*self.mag_r()
@@ -48,6 +124,18 @@ class SnailFish:
             return self.b
         else:
             return self.b.magnitude()
+
+    def val_from_path(self, path):
+        if path is None or len(path) == 0:
+            return self
+        elif path[0] == "L":
+            if isinstance(self.a, int):
+                return self.a if len(path) == 1 else None
+            return self.a.val_from_path(path[1:])
+        else:
+            if isinstance(self.b, int):
+                return self.b if len(path) == 1 else None
+            return self.b.val_from_path(path[1:])
 
     @staticmethod
     def from_string(text, depth = 0):
@@ -80,7 +168,9 @@ class SnailFish:
 
 
 def puzzle1(data):
-    return 0
+    sfs = [SnailFish.from_string(s) for s in data]
+    sf = reduce(lambda a, b: a.add(b), sfs)
+    return sf.magnitude()
 
 
 def puzzle2(data):
@@ -90,5 +180,5 @@ def puzzle2(data):
 if __name__ == '__main__':
     with open("input.txt") as f:
         file = f.read()
-        print(puzzle1(file))
-        print(puzzle2(file))
+        print(puzzle1(file.splitlines()))
+        print(puzzle2(file.splitlines()))
